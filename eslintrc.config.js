3e27ec6b84ae0c1be419935af4c82261b91150e2
Kolja Nolte <kolja.nolte@gmail.com>
2026-05-16 18:20:36 +0700

    feat: add stylelint and eslint configuration files
    - Introduced .stylelintrc.json for CSS linting
    - Added .eslintrc.json for JavaScript linting
    - Renamed .eslintrc.json to eslintrc.config.js with updated rules
    - Updated Settings_Page.php to use new JavaScript and CSS file paths
    - Modified package.json for updated package name and scripts
    
    Signed-off-by: Kolja Nolte <kolja.nolte@gmail.com>

---
+++ eslintrc.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  // ...other config
+  plugins: ["jsdoc"],
+  rules:   {
+    "jsdoc/require-description": "error",
+    "jsdoc/check-values":        "error",
+  },
+  // ...other config
+};
